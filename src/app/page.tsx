66c68d8c45e73600011b8841a6f459cc1d705a31
Amélie Rolland <amelierolland.v@gmail.com>
2024-07-26 14:55:42 +0200

    header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Header from "@/components/header";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
+    <>
     <header>
       <Header />
     </header>
+    <main>
+      <Hero />
+    </main>
+    </>
   );
 }
---
+++ src/components/hero.tsx
@@ -0,0 +1,28 @@
+export default function Hero() {
+    return (
+        <>
+            <section className="container bg-light-pink h-full">
+                <div className="flex h-full lg:w-4/5 mx-auto pt-16">
+                    <div className="flex flex-col justify-center w-full h-full">
+                        <h2>A la une</h2>
+                        <div className="flex flex-row justify-between w-full h-auto">
+                            <div className="w-1/2 bg-gray-200 pe-8">
+
+                            <h3>Nom de l'artiste</h3>
+                            <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda animi officiis nesciunt neque aliquam, eum minus nostrum ipsam ratione, consectetur voluptate nobis optio quae porro laboriosam veniam cumque quis deserunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda animi officiis nesciunt neque aliquam, eum minus nostrum ipsam ratione, consectetur voluptate nobis optio quae porro laboriosam veniam cumque quis deserunt.</p>
+                            
+                            </div>
+                            <div className="w-1/2 bg-gray-300">image</div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </>
+
+
+    )
+}
+
+
+
